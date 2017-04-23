ce1ef7bbebb5ce603c47381af9500a9f83d1c72f
Stockmans, Yannick <stockmans_y@hotmail.com>
2017-04-23 17:26:53 +0200

    Attachments now also show up in the media room

--- NoeS bot/Logic/MediaProcessor.cs
+++ NoeS bot/Logic/MediaProcessor.cs
@@ -24,68 +24,83 @@ namespace NoeSbot.Logic
 
         public async Task Process()
         {
-            var matches = Regex.Matches(_context.Message.Content, @"(www.+|http.+)([\s]|$)");
-            if (matches.Count <= 0)
+            if (!_context.Channel.Name.Equals(Configuration.Load(_context.Guild.Id).GeneralChannel))
                 return;
 
-            if (!_context.Channel.Name.Equals(Configuration.Load(_context.Guild.Id).GeneralChannel))
+            var matches = Regex.Matches(_context.Message.Content, @"(www.+|http.+)([\s]|$)");
+            if (matches.Count <= 0 && !_context.Message.Attachments.Any())
                 return;
 
             var channels = await _context.Guild.GetChannelsAsync();
             var mediaChannel = channels.Where(x => x.Name.Equals(Configuration.Load(_context.Guild.Id).MediaChannel, StringComparison.OrdinalIgnoreCase)).SingleOrDefault() as IMessageChannel;
 
-            if (mediaChannel == null)
-                return;
-                       
+            var messages = mediaChannel.GetMessagesAsync(100, CacheMode.AllowDownload);
+            var flatten = await messages.Flatten();
+
             var user = _context.User as SocketGuildUser;
+
+            if (_context.Message.Attachments.Any())
+            {
+                foreach (var attach in _context.Message.Attachments)
+                {
+                    await ProcessMediaAsync(flatten, attach.Url, user, mediaChannel);
+                }
+            }
+
+            if (matches.Count <= 0 || mediaChannel == null)
+                return;
+
             foreach (Match match in matches)
             {
-                if (CheckForEmbed())//CheckForMedia(match.Value))
-                {
-                    var messages = mediaChannel.GetMessagesAsync(100, CacheMode.AllowDownload);
-                    var flatten = await messages.Flatten();
-
-                    var builder = new StringBuilder();                    
-                    
-                    var isRepost = false;
-                    foreach (var f in flatten) { 
-                        var flattenMsg = Regex.Matches(f.Content, @"(www.+|http.+)([\s]|$)");
-                        if (flattenMsg.Count <= 0)
-                            continue;
-
-                        var mentionedUser = f.Author.Username;
-                        var indexOf = f.Content.IndexOf("posted new");
-                        if (indexOf > -1)
-                            mentionedUser = f.Content.Substring(0, indexOf).Trim();
-
-                        foreach (Match fm in flattenMsg)
-                            if (fm.Value.Equals(match.Value, StringComparison.OrdinalIgnoreCase)){ 
-                                isRepost = true;
-                                break;
-                            }
-
-                        if (isRepost) {
-                            builder.AppendLine("Repost!");
-                            builder.AppendLine($"{mentionedUser} posted the following media on {f.CreatedAt.ToString("dd-MM-yyyy hh:mm")}");
-                            builder.AppendLine("```");
-                            builder.AppendLine($"{match.Value}");
-                            builder.AppendLine("```");
-                            break;
-                        }
-                    }
-
-                    if (!isRepost)
-                    {
-                        builder.AppendLine($"{user.Username} posted new media:");
-                        builder.AppendLine($"{match.Value}");
-                    }
-
-                    await mediaChannel.SendMessageAsync(builder.ToString());
-                }
+                if (CheckForEmbed())//CheckForMedia(match.Value))            
+                    await ProcessMediaAsync(flatten, match.Value, user, mediaChannel);
             }
         }
 
         #region Private
+
+        private async Task ProcessMediaAsync(IEnumerable<IMessage> flatten, string input, SocketGuildUser user, IMessageChannel mediaChannel)
+        {
+            var builder = new StringBuilder();
+
+            var isRepost = false;
+            foreach (var f in flatten)
+            {
+                var flattenMsg = Regex.Matches(f.Content, @"(www.+|http.+)([\s]|$)");
+                if (flattenMsg.Count <= 0)
+                    continue;
+
+                var mentionedUser = f.Author.Username;
+                var indexOf = f.Content.IndexOf("posted new");
+                if (indexOf > -1)
+                    mentionedUser = f.Content.Substring(0, indexOf).Trim();
+
+                foreach (Match fm in flattenMsg)
+                    if (fm.Value.Equals(input, StringComparison.OrdinalIgnoreCase))
+                    {
+                        isRepost = true;
+                        break;
+                    }
+
+                if (isRepost)
+                {
+                    builder.AppendLine("Repost!");
+                    builder.AppendLine($"{mentionedUser} posted the following media on {f.CreatedAt.ToString("dd-MM-yyyy hh:mm")}");
+                    builder.AppendLine("```");
+                    builder.AppendLine($"{input}");
+                    builder.AppendLine("```");
+                    break;
+                }
+            }
+
+            if (!isRepost)
+            {
+                builder.AppendLine($"{user.Username} posted new media:");
+                builder.AppendLine($"{input}");
+            }
+
+            await mediaChannel.SendMessageAsync(builder.ToString());
+        }
 
         private bool CheckForMedia(string input)
         {
